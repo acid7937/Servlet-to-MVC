7d5cae1dde3b572f571918bfd21522043b2a7e05
acid7937 <hsgs5891@gmail.com>
2023-02-09 15:55:20 +0900

    mvc v1~v3

--- src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
@@ -1,2 +1,15 @@
-package hello.servlet.web.frontcontroller.V2;public interface ControllerV2 {
+package hello.servlet.web.frontcontroller.v2;
+
+import hello.servlet.web.frontcontroller.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+
+public interface ControllerV2 {
+
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws SerialException, IOException, ServletException;
+
 }
